e1d46f349e35a5965bf25965d19c148b5bd73d82
Nick Quaranto <nick@quaran.to>
2014-05-14 22:48:39 -0400

    Get real

--- app.rb
+++ app.rb
@@ -1,29 +1,40 @@
 require 'sinatra'
 require 'stripe'
+require 'yaml'
 
 set :publishable_key, ENV['PUBLISHABLE_KEY']
 set :secret_key, ENV['SECRET_KEY']
 
 Stripe.api_key = settings.secret_key
 
+PRODUCTS = YAML.load_file("./products.yml")
+
 get '/' do
+  @products = PRODUCTS
   erb :index
 end
 
-post '/charge' do
-  # Amount in cents
-  @amount = 500
+post '/charge/:product' do
+  @product = PRODUCTS[params[:product]] or raise "Nope!"
 
   customer = Stripe::Customer.create(
-    :email => 'customer@example.com',
+    :email => params[:stripeEmail],
     :card  => params[:stripeToken]
   )
 
   charge = Stripe::Charge.create(
-    :amount      => @amount,
+    :amount      => @product.amount,
     :description => 'CoworkBuffalo Shop',
     :currency    => 'usd',
-    :customer    => customer.id
+    :customer    => customer.id,
+    :metadata    => {
+      :shipping_name    => params[:stripeShippingName],
+      :shipping_address => params[:stripeShippingAddressLine1],
+      :shipping_zip     => params[:stripeShippingAddressZip],
+      :shipping_state   => params[:stripeShippingAddressState],
+      :shipping_city    => params[:stripeShippingAddressCity],
+      :shipping_country => params[:stripeShippingAddressCountry]
+    }
   )
 
   erb :charge
